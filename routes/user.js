71bbb7e6a033fc9b77e955520c43b8fe241f4089
Aidan Farhi <aidanfarhi@Aidans-MacBook-Air.local>
2020-08-19 14:15:10 -0400

    finished part one of wikistack

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,26 @@
 const { Router } = require("express");
+const { Page, User } = require('../models')
+const { userList, userPages } = require('../views')
 
 const router = new Router()
 
-module.exports = router
+router.get('/users', async(req, res, next) => {
+    try {
+        const users = await User.findAll()
+        res.send(userList(users))
+    } catch(err) { next(err) }
+})
 
+router.get('/:userId', async(req, res, next) => {
+    try {
+        const user = await User.findByPk(req.params.userId)
+        const pages = await Page.findAll({
+            where: {
+                authorId: req.params.userId
+            }
+        })
+        res.send(userPages(user, pages))
+    } catch(err) { next(err) }
+})
+
+module.exports = router
--- routes/wiki.js
+++ routes/wiki.js
@@ -1,9 +1,6 @@
 const { Router } = require("express");
 const { Page, User } = require('../models')
-// const { addPage } = require("../views");
-const addPage = require('../views/addPage.js')
-const { wikiPage, main, userList, userPages } = require('../views')
-
+const { wikiPage, main, userList, userPages, addPage } = require('../views')
 
 const router = new Router()
 
@@ -44,23 +41,24 @@ router.get('/:slug', async (req, res, next) => {
     } catch(err) { next(err) }
 });
 
-router.get('/users', async(req, res, next) => {
-    try {
-        const users = await User.findAll()
-        res.send(userList(users))
-    } catch(err) { next(err) }
-})
+// router.get('/users', async(req, res, next) => {
+//     try {
+//         const users = await User.findAll()
+//         console.log(users)
+//         res.send(userList(users))
+//     } catch(err) { next(err) }
+// })
 
-router.get('/:userId', async(req, res, next) => {
-    try {
-        const user = await User.findByPk(req.params.userId)
-        const pages = await Page.findAll({
-            where: {
-                authorId: req.params.userId
-            }
-        })
-        res.send(userPages(user, pages))
-    } catch(err) { next(err) }
-})
+// router.get('/:userId', async(req, res, next) => {
+//     try {
+//         const user = await User.findByPk(req.params.userId)
+//         const pages = await Page.findAll({
+//             where: {
+//                 authorId: req.params.userId
+//             }
+//         })
+//         res.send(userPages(user, pages))
+//     } catch(err) { next(err) }
+// })
 
 module.exports = router
